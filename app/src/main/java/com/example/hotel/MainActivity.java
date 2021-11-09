9e87c3d33ebc24fd5a2f2c5b85e7e4c4e3fb6542
denpopov2002@mail.ru <Den2002321>
2021-11-09 16:24:59 +0300

    Hotel

---
+++ app/src/main/java/com/example/hotel/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nachalolayout);
+    }
+}
---
+++ app/src/main/java/com/example/hotel/nnActivity.java
@@ -0,0 +1,4 @@
+package com.example.hotel;
+
+public class nnActivity {
+}
